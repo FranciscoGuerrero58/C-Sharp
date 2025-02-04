8ea550a4d315a27e7d35367e73d1049d4251bd58
Francisco Guerrero Ferrusca <90232206+FranciscoGuerrero58@users.noreply.github.com>
2025-02-03 18:13:06 -0600

    First projects

---
+++ Calculadora/Calculadora/Program.cs
@@ -0,0 +1,50 @@
+﻿//Calculadora basica en consola 
+//Ing. Francisco Guerrero Ferrusca
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Calculadora en C#");
+        Console.Write("Ingrese el primer número: ");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Ingrese el operador (+, -, *, /): ");
+        char operador = Convert.ToChar(Console.ReadLine());
+
+        Console.Write("Ingrese el segundo número: ");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        double resultado = 0;
+        bool operacionValida = true;
+
+        switch (operador)
+        {
+            case '+':
+                resultado = num1 + num2;
+                break;
+            case '-':
+                resultado = num1 - num2;
+                break;
+            case '*':
+                resultado = num1 * num2;
+                break;
+            case '/':
+                if (num2 != 0)
+                    resultado = num1 / num2;
+                else
+                {
+                    Console.WriteLine("Error: División por cero no permitida.");
+                    operacionValida = false;
+                }
+                break;
+            default:
+                Console.WriteLine("Error: Operador no válido.");
+                operacionValida = false;
+                break;
+        }
+
+        if (operacionValida)
+            Console.WriteLine($"Resultado: {resultado}");
+    }
+}
---
+++ CalculadoraCientifica/CalculadoraCientifica/Program.cs
@@ -0,0 +1,60 @@
+﻿//Calculadora con + - * / y ^2 
+
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Lee correctamente las instrucciones");
+        Console.WriteLine("Ingresa el primer número");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("¿Qué operación deseas elegir? +, -, *, /, ^ ");
+        char oper = Convert.ToChar(Console.ReadLine());
+
+        double result = 0;
+        bool validOperation = true;
+
+        if (oper == '^')
+        { result = Math.Pow(num1, 2); }
+        else
+        {
+            Console.WriteLine("Ingresa el segundo número");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+
+
+            switch (oper)
+            {
+                case '+':
+                    result = num1 + num2;
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    break;
+                case '/':
+                    if (num1 != 0)
+                        result = num1 / num2;
+                    else
+                    {
+                        Console.WriteLine("Error, división entre 0");
+                        validOperation = false;
+                    }
+                    break;
+
+
+                default:
+                    Console.WriteLine("Error: operador no valido");
+                    validOperation = false;
+                    break;
+
+            }
+        }
+        if (validOperation)
+        {
+            Console.WriteLine($"El resultado es: {result}");
+        }
+    }
+}
